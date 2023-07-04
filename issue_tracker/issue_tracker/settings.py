ed15a67b2985c22557136031f74cade9983025e1
sahilkumar003 <kumarsahil944795@gmail.com>
2023-07-04 17:29:16 +0530

    somem changes in the db

--- issue_tracker/issue_tracker/settings.py
+++ issue_tracker/issue_tracker/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = "issue_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "jira",
-        "USER": "postgres",
-        "PASSWORD": "Psqlitsmesahil@123",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
